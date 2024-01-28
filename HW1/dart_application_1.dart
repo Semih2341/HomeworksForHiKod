e0b1bfc98848e2f43c2615c9206e39e9ff400516
Semih2341 <semih1423tr@gmail.com>
2024-01-28 19:27:11 +0300

    HW1DONE

---
+++ HW1/dart_application_1.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: unused_import
+
+import 'dart:ffi';
+import 'dart:math';
+
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+// void main() {
+//   print("Hi Kod");
+// }
+
+// void main() {
+//   String name = "Semih";
+//   int age = 22;
+//   double height = 178;
+//   bool isStudent = false;
+
+//   print("Name: $name");
+//   print("Age: $age");
+//   print("Height: $height cm");
+
+//   // ignore: dead_code
+//   if (isStudent) {
+//     print("He is a student");
+//   } else {
+//     print("He is not a student");
+//   }
+// }
+
+// void main() {
+//   int a = 10;
+//   int b = 5;
+//   int c = 3;
+
+//   int sum = a + b;
+//   int difference = a - b;
+//   int product = a * b;
+//   double quotient = a / b;
+//   double askedfor = ((a - b) * c / (a + b)) * (a * b * c);
+
+//   print("Sum: $sum");
+//   print("Difference: $difference");
+//   print("Product: $product");
+//   print("Quotient: $quotient");
+//   print("Asked for: $askedfor");
+// }
+
+// void main() {
+//   int charge = 141;
+
+//   if (charge == 100) {
+//     print("Telefon Şarj Edildi");
+//   } else if (charge >= 10 && charge < 20) {
+//     print("Telefonunuzu şarj edin");
+//   } else if (charge < 10) {
+//     print("Kritik batarya seviyesi");
+//   } else {
+//     print("Telefon uzaydan geldi herhalde!");
+//   }
+// }
+
+// void main() {
+//   greeting("Semih", 22, 178, "Yeşil");
+// }
+
+// void greeting(String name, int age, double height, String FavoriteColor) {
+//   print(
+//       "Merhaba, ben $name, $age yaşındayım. boyum $height ve en sevdiğim renk $FavoriteColor");
+// }
